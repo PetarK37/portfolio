f276607c6b699e4d8e3934217301715bb0fc9f41
Petar Komordžić <petar.komordzic@gmail.com>
2025-08-06 09:32:19 +0200

    fix: minor ui refactor

--- src/components/layout/AboutSection.tsx
+++ src/components/layout/AboutSection.tsx
@@ -23,29 +23,29 @@ function AboutSection() {
                     >
                         <h2 className="h-fit text-left text-4xl font-bold">Who Am I?</h2>
                         <p className="mt-4 text-left text-lg w-full font-semibold text-text_secondary lg:max-w-[80%]">
-                            My name is Petar, and I am a 23-year-old full-stack developer.
+                            My name is Petar, and I am a {new Date().getFullYear() - 2001}-year old full-stack developer.
                             Ever since I got my first PC at the age of 5, I’ve known I wanted
-                            to work in the tech world. I was always the <b>"IT guy"</b> in my
+                            to work in the tech world. I was always the <strong>"IT guy"</strong> in my
                             family and even ran an improvised PC and electronics repair shop
                             with my best friend.
                         </p>
                         <p className="mt-4 text-left text-lg w-full font-semibold text-text_secondary lg:max-w-[80%]">
                             Beyond technology, I’ve explored a variety of hobbies. I was an
                             active member of the{" "}
-                            <b>"Amatersko Pozorište Janko Veselinović"</b> theatre group,
+                            <strong>"Amatersko Pozorište Janko Veselinović"</strong> theatre group,
                             earning quite a few medals at amateur festivals across the
                             country. I’ve also dabbled in poetry recitation, hosting local
                             festivals, and even danced traditional Serbian folklore during
                             middle school. Oh, and I used to practice swimming too!
                         </p>
                         <p className="mt-4 text-left text-lg w-full font-semibold text-text_secondary lg:max-w-[80%]">
-                            These days, I enjoy <b>traveling</b> and immersing myself in new
+                            These days, I enjoy <strong>traveling</strong> and immersing myself in new
                             places and cultures. Sipping coffee at cozy cafes or trying out
                             new cuisines is something I’m always up for. A good movie marathon
-                            is another favorite activity of mine. <b>Fun fact:</b> I once
+                            is another favorite activity of mine. <strong>Fun fact:</strong> I once
                             reserved so many movies on an movie theatre app that my account
                             got flagged! Recently, I’ve started learning{" "}
-                            <b>electric guitar and German</b>—let’s see how those two go :)
+                            <strong>electric guitar and German</strong>—let’s see how those two go :)
                         </p>
                         <p className="mt-4 text-left text-lg w-full font-semibold text-text_secondary lg:max-w-[80%]">
                             I’d love to collaborate, share ideas, or just chat about tech and
@@ -150,7 +150,7 @@ function AboutSection() {
                                     </h3>
                                     <p className="h-fit font-normal text-base text-center">
                                         Got Professional Bachelor Of Software Degree with grade{" "}
-                                        <b>9.64</b>
+                                        <strong>9.64</strong>
                                     </p>
                                     <p className="h-fit text-center text-lg font-bold mt-auto">
                                         2020 - 2023
