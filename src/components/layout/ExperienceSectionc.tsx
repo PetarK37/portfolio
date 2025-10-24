73258f4f8e1973d87e072242ab967975d4677c5d
PetarK37 <komordzic.sr37.2020@uns.ac.rs>
2025-10-24 18:05:02 +0200

    Added new job experience

--- src/components/layout/ExperienceSectionc.tsx
+++ src/components/layout/ExperienceSectionc.tsx
@@ -4,6 +4,7 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import vegaLogo from '../../assets/images/VegaItLogo.jpg'
 import fortixLogo from '../../assets/images/FortixTechLogo.png'
 import zenithLogo from '../../assets/images/ZenithChatLogo.png'
+import tubeiqLogo from '../../assets/images/logo-tubeiq.png'
 import 'react-vertical-timeline-component/style.min.css';
 
 interface Job {
@@ -18,11 +19,19 @@ interface Job {
 
 const jobs: Job[] = [
     {
+        title: "Tubeiq",
+        dateStr: "October 205 - Today",
+        desc: "Part of a .NET development team in a Tubeiq (member of an Egzakta group)",
+        icon: <img src={tubeiqLogo} style={{ objectFit: 'cover', height: '100%' }} />,
+        position: 'left',
+        role: "Junior Software Engineer"
+    },
+    {
         title: "ZenithChat",
         dateStr: "January 2024 - August 2025",
         desc: "As a lead developer for Zenith Chat I was responsible for creating and fine-tuning a cutting-edge Instagram chatbot capable of replacing all repetitive human interactions in their dm's. \n This project has taught me many new and interesting technologies from Python + React all the way to LLM's, embeddings, vector databases etc…",
         icon: <img src={zenithLogo} style={{ objectFit: 'cover' }} />,
-        position: 'left',
+        position: 'right',
         role: "Lead Software Engineer"
     },
     {
@@ -30,7 +39,7 @@ const jobs: Job[] = [
         dateStr: "July 2023 - August 2025",
         desc: "Worked in a small team on outsourcing projects. Worked on veracity projects with and without AI integration for medium and large size corporations all around the world. This team lead to the creation of ZenithChat - The ultimate Instagram messaging assistant!",
         icon: <img src={fortixLogo} />,
-        position: 'right',
+        position: 'left',
         role: "Full-stack developer",
         bgcolor: "#fff"
     },
@@ -39,7 +48,7 @@ const jobs: Job[] = [
         dateStr: "August 2022 - August 2022",
         desc: "Worked on project in .Net core and React frontend. Learned about clean architecture and best practices in that technologies, but also in development overall.",
         icon: <img src={vegaLogo} />,
-        position: 'left',
+        position: 'right',
         role: "Software Engineer Intern"
     },
 ]
